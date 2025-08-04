06de3a253ad0cf054efb72fa523a01fc8388ba69
Michael Freimüller <michael@mfreimueller.com>
2025-08-04 14:43:47 +0200

    use jakarta validation for CreateUserDto

--- UserService/src/main/java/com/mfreimueller/dto/CreateUserDto.java
+++ UserService/src/main/java/com/mfreimueller/dto/CreateUserDto.java
@@ -1,7 +1,11 @@
 package com.mfreimueller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+
 import java.time.LocalDate;
 
-public record CreateUserDto(String firstName, String lastName, String email, LocalDate birthday) {
+public record CreateUserDto(@NotEmpty String firstName, @NotEmpty String lastName, @Email String email, @Past LocalDate birthday) {
 
 }

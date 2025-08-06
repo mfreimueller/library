4346d017267c326d6696bbf5b1b51235537aab8e
Michael Freimüller <michael@mfreimueller.com>
2025-08-06 11:45:09 +0200

    update BorrowedBookMapper by removing Converter

--- borrow-service/src/main/java/com/mfreimueller/mapper/BorrowedBookMapper.java
+++ borrow-service/src/main/java/com/mfreimueller/mapper/BorrowedBookMapper.java
@@ -1,11 +1,13 @@
 package com.mfreimueller.mapper;
 
 import com.mfreimueller.domain.BorrowedBook;
+import com.mfreimueller.dto.BookDto;
 import com.mfreimueller.dto.BorrowedBookDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.core.convert.converter.Converter;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface BorrowedBookMapper extends Converter<BorrowedBook, BorrowedBookDto> {
+@Mapper(componentModel = "spring")
+public interface BorrowedBookMapper {
+    BorrowedBookDto toDto(BorrowedBook borrowedBook);
 }

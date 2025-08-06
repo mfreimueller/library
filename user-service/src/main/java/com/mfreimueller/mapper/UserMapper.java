09e0ead3d5b6655d609c7d5259df4d6484f492c9
Michael Freimüller <michael@mfreimueller.com>
2025-08-06 10:27:56 +0200

    remove unused import

--- user-service/src/main/java/com/mfreimueller/mapper/UserMapper.java
+++ user-service/src/main/java/com/mfreimueller/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.mfreimueller.mapper;
 import com.mfreimueller.domain.User;
 import com.mfreimueller.dto.UserDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 import org.springframework.core.convert.converter.Converter;
 
 @Mapper(componentModel = "spring")
